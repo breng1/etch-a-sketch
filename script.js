34efabc40b69939d5dc0b89e554761b055a78348
breng1 <brendaknguyen@gmail.com>
2022-10-16 19:05:19 -0700

    Add dynamically-sized square grid
    Add makeGrid function to script.js which creates a grid with
    a specified number of rows and columns, using the CSS Grid Layout
    Module.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // script.js
+
+const GRID_AREA = 400;
+
+// Creates square grid with 'size' number of rows and columns
+function makeGrid (size) {
+    let container = document.querySelector(".grid-container");
+
+    // Set rows, columns, and cell size
+    let cellArea = GRID_AREA/size;
+    container.style.gridTemplateRows = `repeat(${size}, ${cellArea}px)`;
+    container.style.gridTemplateColumns = `repeat(${size}, ${cellArea}px)`;
+    
+    // Generate grid items
+    for (let i = 1; i <= size**2; i++) {
+        let item = document.createElement('div');
+        item.classList.add(`grid-item`);
+        //item.textContent = `${i}`;
+        container.appendChild(item);
+    }
+}
+
+makeGrid(16);
